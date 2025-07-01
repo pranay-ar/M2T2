a58e7eaaf1c28b36974d497af06f679d57ae64b3
pranay.ar <a.pranay2001@gmail.com>
2025-07-01 10:47:13 -0700

    update requirements

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import glob
+import os
 import os.path as osp
 
-_ext_src_root = osp.join("pointnet2_ops", "_ext-src")
+# Get the absolute path to the project root
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+_ext_src_root = osp.join("pointnet2_ops", "pointnet2_ops", "_ext-src")
 _ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
     osp.join(_ext_src_root, "src", "*.cu")
 )
 
-exec(open(osp.join("pointnet2_ops", "pointnet2_ops", "_version.py")).read())
+with open(osp.join("pointnet2_ops", "pointnet2_ops", "_version.py")) as f:
+    exec(f.read())
 
 setup(
     name="m2t2",
     version="1.0.0",
-    packages=find_packages() + find_packages(where='pointnet2_ops'),
-    package_dir={'pointnet2_ops': 'pointnet2_ops/pointnet2_ops'},
+    author="Wentao Yuan",
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         line for line in open('requirements.txt').readlines()
@@ -28,12 +33,12 @@ setup(
                 "cxx": ["-O3"],
                 "nvcc": ["-O3", "-Xfatbin", "-compress-all"],
             },
-            include_dirs=[osp.join(osp.dirname(osp.abspath(__file__)), _ext_src_root, "include")],
+            # Use an absolute path for the include directory
+            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
         )
     ],
     cmdclass={"build_ext": BuildExtension},
     description="Multi-Task Masked Transformer",
-    author="Wentao Yuan",
     author_email="wentaoy@nvidia.com",
     license="MIT Software License",
     url="https://m2-t2.github.io",
